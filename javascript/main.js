7743b3a7dcf4d18cc8961cc5dfd3808398a74c23
daniel.weck <daniel.weck@f32bda36-2e0e-725c-97ed-abc4eacfd11b>
2010-12-27 00:32:08 +0000

    minor reorganization
    git-svn-id: http://html-scrabble.googlecode.com/svn/trunk@4 f32bda36-2e0e-725c-97ed-abc4eacfd11b

--- javascript/main.js
+++ javascript/main.js
@@ -17,23 +17,63 @@ Similar HTML/Javascript project:
 http://www.themaninblue.com/writing/perspective/2004/01/27/
 */
 
+// BEGIN script-scope ------------------
+(function(){
+
 var _OBJECT_ROOT_ = window;
 
 
+//=================================================
+// BEGIN Namespace util ------------------
+//var HTMLScrabble = (typeof HTMLScrabble == "undefined" || !HTMLScrabble ) ? {} : HTMLScrabble;
+//var HTMLScrabble.UI = (typeof HTMLScrabble.UI == "undefined" || !(HTMLScrabble.UI) ) ? {} : HTMLScrabble.UI;
+/*
+console.log("Checking namespace: " + "HTMLScrabble.UI");
+EnsureNamespace("HTMLScrabble.UI");
+
+console.log("Checking namespace: " + "HTMLScrabble.Core");
+EnsureNamespace("HTMLScrabble.Core");
+*/
+function EnsureNamespace(nsString)
+//_OBJECT_ROOT_.EnsureNamespace = function(nsString)
+{
+	var nsStrings = nsString.split(".");
+	var root = _OBJECT_ROOT_;
+	for (var i = 0; i < nsStrings.length; i++)
+	{
+		var nsName = nsStrings[i];
+		var val = root[nsName];
+		if (typeof val == "undefined" || !val)
+		{
+			console.log("Creating namespace object: " + nsName);
+			root[nsName] = new Object(); // {} ?
+		}
+		else
+		{
+			console.log("Namespace object already exists: " + nsName);
+		}
+		root = root[nsName];
+	}
+}
+// END Namespace util ------------------
+//=================================================
+
+//=================================================
 // BEGIN Events ------------------
-_OBJECT_ROOT_.Events = (function(){
+if (typeof _OBJECT_ROOT_.EventsManager == "undefined" || !_OBJECT_ROOT_["EventsManager"])
+_OBJECT_ROOT_.EventsManager = (function(){
 
 //Events.AddEventListener('provide', function() {});
 //Events.Dispatch('provide', { 'identifier': identifier });
 
-var _Events = {}; // 'this' object (to be returned at the bottom of the containing auto-evaluated "Events" function)
+var _EventsManager = {}; // 'this' object (to be returned at the bottom of the containing auto-evaluated "EventsManager" function)
 
 var _EventListeners = {}; // private associative array (event name -> array of callback functions)
 
 // eventName ==> string
 // eventPayload ==> can be anything, but usually a simple associative array such as:
 // { 'key1': "value1", 'key2': "value2" }
-_Events.Dispatch = function(eventName, eventPayload)
+_EventsManager.DispatchEvent = function(eventName, eventPayload)
 {
 	var callbacks = _EventListeners[eventName]; // simple indexed array of callback functions
 	if (typeof callbacks == "undefined" || !callbacks)
@@ -65,7 +105,7 @@ _Events.Dispatch = function(eventName, eventPayload)
 	}
 };
 
-_Events.AddEventListener = function(eventName, callback)
+_EventsManager.AddEventListener = function(eventName, callback)
 {
 	var callbacks = _EventListeners[eventName]; // simple indexed array of callback functions
 	if (typeof callbacks == "undefined" || !callbacks)
@@ -88,7 +128,7 @@ _Events.AddEventListener = function(eventName, callback)
 	callbacks.push(callback);
 };
 
-_Events.RemoveEventListener = function(eventName, callback)
+_EventsManager.RemoveEventListener = function(eventName, callback)
 {
 	var callbacks = _EventListeners[eventName]; // simple indexed array of callback functions
 	if (typeof callbacks == "undefined" || !callbacks)
@@ -125,62 +165,20 @@ _Events.RemoveEventListener = function(eventName, callback)
 		console.log("Event.RemoveEventListener - callback for eventName not found: " + eventName + ", " + callback);
 };
 
-return _Events;
+return _EventsManager;
 })();
 // END Events ------------------
+//=================================================
 
 
-// BEGIN script-scope ------------------
-//_OBJECT_ROOT_.HTMLScrabble_ =
-(function(){
 
-function EnsureNamespace(nsString)
-{
-	var nsStrings = nsString.split(".");
-	var root = _OBJECT_ROOT_;
-	for (var i = 0; i < nsStrings.length; i++)
-	{
-		var nsName = nsStrings[i];
-		var val = root[nsName];
-		if (typeof val == "undefined" || !val)
-		{
-			console.log("Creating namespace object: " + nsName);
-			root[nsName] = new Object(); // {} ?
-		}
-		else
-		{
-			console.log("Namespace object already exists: " + nsName);
-		}
-		root = root[nsName];
-	}
-}
 
-//var HTMLScrabble = (typeof HTMLScrabble == "undefined" || !HTMLScrabble ) ? {} : HTMLScrabble;
-//var HTMLScrabble.UI = (typeof HTMLScrabble.UI == "undefined" || !(HTMLScrabble.UI) ) ? {} : HTMLScrabble.UI;
-
-console.log("Checking namespace: " + "HTMLScrabble.UI");
-EnsureNamespace("HTMLScrabble.UI");
-
-console.log("Checking namespace: " + "HTMLScrabble.Core");
-EnsureNamespace("HTMLScrabble.Core");
-
+/*
 HTMLScrabble.UI.test = function()
 {
 	return _sf_startpt; // defined in HTML page
 }
-
-/*
-testClass = function()
-{
-	this.testMember = "hello";
-}
-testClass.prototype.Member = function()
-{
-	return this.testMember;
-}
 */
-
-return HTMLScrabble;
 
 })();
 // END script-scope ------------------
@@ -188,22 +186,26 @@ return HTMLScrabble;
 
 window.onload = function()
 {
-var callback = function(properties)
+
+var callback = function(eventPayload)
 	{
-		//for (var i = 0; i < properties.length; i++)
-		for (var key in properties)
+		//for (var i = 0; i < eventPayload.length; i++)
+		for (var key in eventPayload)
 		{
-			alert(key + "," + properties[key]);
+			alert(key + "," + eventPayload[key]);
 		}
 	};
-Events.AddEventListener('EVENT_NAME', callback);
-Events.Dispatch('EVENT_NAME', { 'key1': "value1", 'key2': "value2" });
+with (EventsManager)
+{
+AddEventListener('EVENT_NAME', callback);
+DispatchEvent('EVENT_NAME', { 'key1': "value1", 'key2': "value2" });
 
-Events.AddEventListener('EVENT_NAME', callback);
-Events.Dispatch('EVENT_NAME', { '-key1': "-value1", '-key2': "-value2" });
+AddEventListener('EVENT_NAME', callback);
+DispatchEvent('EVENT_NAME', { '-key1': "-value1", '-key2': "-value2" });
 
-Events.RemoveEventListener('EVENT_NAME', callback);
-Events.Dispatch('EVENT_NAME', { '_key1': "_value1", '_key2': "_value2" });
+RemoveEventListener('EVENT_NAME', callback);
+DispatchEvent('EVENT_NAME', { '_key1': "_value1", '_key2': "_value2" });
+}
 
 /*
 alert("1: " + HTMLScrabble.UI.test());
