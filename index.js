507c23c00f10186a4acfcfd0be04ab0b829bdd12
Hans Huebner <hans.huebner@gmail.com>
2012-06-30 12:22:50 +0200

    externalize initialization script

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var game = 0;
+var board = 0;
+var rack = 0;
+var boardUI = 0;
+
+$(document).ready(function() {
+    boardUI = new window.Scrabble.UI.Html(); // must be instantiated first (to listen to Core events)
+
+    board = new window.Scrabble.Core.Board();
+    rack = new window.Scrabble.Core.Rack();
+
+    game = new window.Scrabble.Core.Game(board, rack);
+
+});
