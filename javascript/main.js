e9edf182a9b104cb8d33aa61b4b30a4fb57c62e3
Hans Huebner <hans.huebner@gmail.com>
2012-06-30 19:42:30 +0200

    remove unused functions

--- javascript/main.js
+++ javascript/main.js
@@ -1539,23 +1539,4 @@ UI.prototype.SetCurrentlySelectedSquareUpdateTargets = function(square) {
     }
 }
 
-function randomInt(N) {
-    // % 1 is needed because some implementations of Math.random() can
-    // actually return 1 (early version of Opera for example).
-    // | 0 does the same as Math.floor() would here, but is probably
-    // slightly quicker.
-    // For details, see: http://www.merlyn.demon.co.uk/js-randm.htm
-    return (N * (Math.random() % 1)) | 0;
-}
-
-function getCookie(cookieName) {
-    var theCookie = document.cookie;
-    if (!theCookie) return 0;
-    var cookies = theCookie.split("; ");
-    for (var i = 0; i < cookies.length; ++i) {
-	var nameVal = cookies[i].split("=");
-	if (nameVal[0] == cookieName) return nameVal[1];
-    }
-    return 0;
-}
 
